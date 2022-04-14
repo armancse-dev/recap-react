a22889ec5cecfe51df67c8abf0e630887d78e5c1
armancse-dev <armanhossaincse7@gmail.com>
2022-04-14 12:53:09 +0600

    Check out Event listener, useState inside components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -13,11 +14,16 @@ function App() {
 }
 
 function MyComponent(props){
-  console.log(props);
+  const [points, setPoints] = useState(1);
+  const handlePoints = () => {
+    const newPoints = points * 2;
+    setPoints(newPoints);
+  }
   return(
     <div className='myStyle'>
       <h1>Yo yo mama ! {props.brand}</h1>
-      <h4>Asking Money: {props.price}</h4>
+      <h4>Asking Money: {props.price}, I have points: {points}</h4>
+      <button onClick={handlePoints}>Add Points</button>
       <p>i can write my own component</p>
     </div>
   )
